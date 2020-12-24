79e94bab2a7e0604f74492a24082da1efdd2048a
carsonhuang <981347807@qq.com>
2020-12-25 00:06:43 +0800

    项目结构划分，路由配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React, { memo } from 'react'
+import { HashRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+
+import routes from '@/router'
+
+import WDAppHeader from '@/components/app-header'
+import WDAppFooter from '@/components/app-footer'
 
 export default memo(function App() {
-  return <div>App</div>
+  return (
+    <HashRouter>
+      <WDAppHeader />
+      {renderRoutes(routes)}
+      <WDAppFooter />
+    </HashRouter>
+  )
 })
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import WDDiscover from '@/pages/discover'
+import WDMine from '@/pages/mine'
+import WDFriend from '@/pages/friend'
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: WDDiscover,
+  },
+  {
+    path: '/mine',
+    component: WDMine,
+  },
+  {
+    path: '/friend',
+    component: WDFriend,
+  },
+]
+
+export default routes
