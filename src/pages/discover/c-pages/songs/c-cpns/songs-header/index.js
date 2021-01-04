ca1e5109936b16d1f7dec6620f8696509ebcaad2
carsonhuang <981347807@qq.com>
2021-01-04 18:58:21 +0800

    完成歌单页面的顶部部分

--- src/pages/discover/c-pages/songs/c-cpns/songs-header/index.js
+++ src/pages/discover/c-pages/songs/c-cpns/songs-header/index.js
@@ -1,7 +1,27 @@
 import React, { memo } from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
 
-import { SongsHeaderWrapper } from './style'
+import { SongsHeaderWrapper, SongsHeaderLeft, SongsHeaderRight } from './style'
 
 export default memo(function WDSongsHeader() {
-  return <SongsHeaderWrapper>WDSongsHeader</SongsHeaderWrapper>
+  const { currentCategory } = useSelector((state) => {
+    return {
+      currentCategory: state.getIn(['songs', 'currentCategory']),
+    }
+  }, shallowEqual)
+
+  return (
+    <SongsHeaderWrapper>
+      <SongsHeaderLeft>
+        <span className="title">{currentCategory}</span>
+        <button className="select">
+          <span>选择分类</span>
+          <i className="sprite_icon2"></i>
+        </button>
+      </SongsHeaderLeft>
+      <SongsHeaderRight>
+        <button className="hot text_hover">热门</button>
+      </SongsHeaderRight>
+    </SongsHeaderWrapper>
+  )
 })
--- src/pages/discover/c-pages/songs/c-cpns/songs-header/style.js
+++ src/pages/discover/c-pages/songs/c-cpns/songs-header/style.js
@@ -1,3 +1,58 @@
 import styled from 'styled-components'
 
-export const SongsHeaderWrapper = styled.div``
+export const SongsHeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #c20c0c;
+  padding-bottom: 6px;
+`
+
+export const SongsHeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  .title {
+    font-size: 24px;
+    font-family: 'Microsoft Yahei', Arial, Helvetica, sans-serif;
+  }
+
+  .select {
+    position: relative;
+    top: 2px;
+    width: 91px;
+    height: 31px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fafafa;
+    border: 1px solid #d3d3d3;
+    color: #0c73c2;
+    margin-left: 10px;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:hover {
+      background: #ffffff;
+    }
+
+    i {
+      margin-left: 5px;
+      display: inline-block;
+      width: 8px;
+      height: 5px;
+      background-position: -70px -543px;
+    }
+  }
+`
+
+export const SongsHeaderRight = styled.div`
+  .hot {
+    width: 46px;
+    height: 29px;
+    color: #ffffff;
+    background-color: #c20c0c;
+    border-radius: 3px;
+  }
+`
